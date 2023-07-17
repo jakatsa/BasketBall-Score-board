16b570087aaa75d78ff68494fe6104e1898d935c
jakatsa <118554412+jakatsa@users.noreply.github.com>
2023-07-17 14:53:06 +0300

    Added files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+let countEl=document.getElementById("Count-El")
+let countLE=document.getElementById("Count-LE")
+let count=0;
+let counts=0
+let Home=document.getElementById("Home")
+let Away=document.getElementById("Away")
+
+
+
+function one(){
+  
+    count=count + 1
+    countEl.innerText=count
+   
+  
+ 
+}
+
+function ones(){
+  
+    counts=counts + 1
+  
+    countLE.innerText=counts
+  
+ 
+}
+
+function two(){
+  
+    count=count + 2
+    countEl.innerText=count
+   
+  
+ 
+}
+function twos(){
+  
+    counts=counts + 2
+  
+    countLE.innerText=counts
+  
+ 
+}
+
+function three(){
+    
+    count=count + 3
+   countEl.innerText=count
+   
+   
+
+}
+function threes(){
+  
+    counts=counts + 3
+  
+    countLE.innerText=counts
+  
+ 
+}
+/*
+function save(){
+    Away.innerText="Away Count: " +  counts
+    Home.innerText="Home Count: "+ count
+
+}*/
+
+/*
+function two(){
+    
+    count=count + 2
+    console.log(count)
+   
+  
+
+
+}
+
+
+
+
+
+*/
+
+
